06c67db47310e4a2f535ee104ad28e73b44d876c
Archisha Chatterjee <archisha.chatterjee.btech2024@sitpune.edu.in>
2025-10-07 19:53:10 +0530

    Create EXP15C.cpp

---
+++ EXP15C.cpp
@@ -0,0 +1,31 @@
+// EXP 15C : Reverse Number
+
+#include <iostream>
+#include <string>
+using namespace std;
+
+void num_reverse(int num) {
+    if(num > 0){
+        cout << num % 10;
+        num_reverse(num/10);
+    }
+}
+
+int main() {
+    int number;
+    cout<<"Enter a number :"<<"\t";
+    cin>>number;
+
+
+    // Function call
+    cout<<"Reversed Number : "<<"\t";
+    num_reverse(number);
+
+    return 0;
+}
+
+/*
+Sample Output : 
+Enter a number :    2342525
+Reversed Number :   5252432
+*/
